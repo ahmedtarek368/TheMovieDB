43f4bd39135b9eb57cf528cabd45386f27416cb0
ahmedtarek <ahmedtarek368@hotmail.com>
2022-01-30 14:43:28 +0200

    Grey nav bar problem on iOS 15 fixed

--- TheMovieDB/TheMovieDB/Files/CustomizedClasses/CustomNavBar.swift
+++ TheMovieDB/TheMovieDB/Files/CustomizedClasses/CustomNavBar.swift
@@ -11,14 +11,12 @@ extension UINavigationBar{
     
     func fullTranslucent(){
         setBackgroundImage(UIImage(), for:.default)
-        backgroundColor = .clear
-        isTranslucent = true
     }
     
     func darkTranslucent(){
         setBackgroundImage(UIImage(named: "black1.jpg"), for:.default)
         shadowImage = UIImage()
-        backgroundColor = #colorLiteral(red: 0.6666666667, green: 0.6666666667, blue: 0.6666666667, alpha: 1)
-        isTranslucent = false
+        backgroundColor = .clear
+        isTranslucent = true
     }
 }
